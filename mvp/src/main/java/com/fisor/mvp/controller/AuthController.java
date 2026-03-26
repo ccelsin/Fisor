7e8d198cd0b7f5453d2bfd0e4dfc0a4675d8b952
ccelsin <celsinadjagboni@gmail.com>
2026-03-26 12:38:28 +0100

    [ADD] Inscription utilisateur

--- mvp/src/main/java/com/fisor/mvp/controller/AuthController.java
+++ mvp/src/main/java/com/fisor/mvp/controller/AuthController.java
@@ -3,11 +3,14 @@ package com.fisor.mvp.controller;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import com.fisor.mvp.dto.JwtAuthResponse;
 import com.fisor.mvp.dto.LoginDto;
+import com.fisor.mvp.dto.UserDto;
 import com.fisor.mvp.service.AuthService;
+import com.fisor.mvp.service.CustomUserDetailsService;
 
 @AllArgsConstructor
 @RestController
@@ -15,6 +18,7 @@ import com.fisor.mvp.service.AuthService;
 public class AuthController {
 
     private AuthService authService;
+    private CustomUserDetailsService userDetailsService;
 
     // Build Login REST API
     @PostMapping("/login")
@@ -30,8 +34,10 @@ public class AuthController {
 
 
     
-    /*@PostMapping("/signin")
-    public ResponseEntity<?> signin(@RequestBody UserDto){
-        
-    }*/
+    @PostMapping("/signin")
+    public ResponseEntity<?> signin(@RequestBody UserDto userDto){
+        userDetailsService.saveUser(userDto);
+        return new ResponseEntity<>(userDto, HttpStatus.CREATED);
+
+    }
 }
--- mvp/src/main/java/com/fisor/mvp/service/CustomUserDetailsService.java
+++ mvp/src/main/java/com/fisor/mvp/service/CustomUserDetailsService.java
@@ -6,11 +6,17 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.fisor.mvp.dto.UserDto;
+import com.fisor.mvp.model.Role;
 import com.fisor.mvp.model.User;
+import com.fisor.mvp.repository.RoleRepository;
 import com.fisor.mvp.repository.UserRepository;
 
+import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -19,6 +25,9 @@ import java.util.stream.Collectors;
 public class CustomUserDetailsService implements UserDetailsService {
 
     private UserRepository userRepository;
+    private RoleRepository roleRepository;
+    private PasswordEncoder passwordEncoder;
+
 
     @Override
     public UserDetails loadUserByUsername(String usernameOrEmail) throws UsernameNotFoundException {
@@ -37,7 +46,34 @@ public class CustomUserDetailsService implements UserDetailsService {
         );
     }
 
-    /*public UserDetails saveUser(String username, String email, String password){
+    public UserDetails saveUser(UserDto userDto){
+        Optional<User> optionalUser = userRepository.findByUsernameOrEmail(userDto.getUsername(), userDto.getEmail());
+        Role userRole = roleRepository.findByName("ROLE_USER");
+        User user = new User();
+        Set<Role> userRoles = new HashSet<>();
+        if(userRole != null){
+                userRoles.add(userRole);
+        }
+        if(optionalUser.isPresent()){
+                throw new RuntimeException("User already exists");
+        }
 
-    }*/
+        
+        
+        user.setUsername(userDto.getUsername());
+        user.setEmail(userDto.getEmail());
+        user.setPassword(passwordEncoder.encode(userDto.getPassword()));
+        user.setRoles(userRoles);
+        user = userRepository.save(user);
+        Set<GrantedAuthority> authorities = user.getRoles().stream()
+        .map((role) -> new SimpleGrantedAuthority(role.getName()))
+        .collect(Collectors.toSet());
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                authorities
+                
+        );
+
+    }
 }
